2267c807f82b4f5f65506b202d0900225609bf7c
Sravani Valligari <126823877+sravanivalligari@users.noreply.github.com>
2023-06-21 13:26:19 -0500

    New R shiny draft
    Modified scatter plot into boxplot. 
    Boxplot with x-axis traits and y-axis genecount
    Histogram with x-axis genecount
    Added custom palatte

--- Rshiny Application/rshinydraft.R
+++ Rshiny Application/rshinydraft.R
@@ -1,96 +1,117 @@
 library(shiny)
 library(readr)
+library(ggplot2)
 library(plotly)
-library(shinydashboard)
-library(reactable)
-library(shinyWidgets)
+library(dplyr)
 
-# Load the data outside of the server function
-snps_subset <- read_csv("C:/Users/srava/Desktop/Intern/snps_subset.csv")
-
-# Get unique species from the data
-unique_species <- unique(snps_subset$org)
-
-# Define UI
-ui <- dashboardPage(
-  dashboardHeader(title = "SNPs Subset Viewer"),
-  dashboardSidebar(
-    selectInput("sort", "Sort by:", choices = colnames(snps_subset)),
-    numericInput("rows", "Number of rows to display:", min = 1, max = nrow(snps_subset), value = 10),
-    pickerInput("filter_trait", "Filter by Trait:", choices = unique(snps_subset$trait), multiple = TRUE, options = list(`actions-box` = TRUE)),
-    pickerInput("filter_species", "Filter by Species:", choices = unique_species, multiple = TRUE, options = list(`actions-box` = TRUE)),
-    selectInput("chromosome", "Filter by Chromosome:", choices = unique(snps_subset$chr))
+# User interface
+# User interface
+ui <- fluidPage(
+  tags$head(
+    tags$style(
+      type = "text/css",
+      ".scrollable_plot {
+        height: 700px;
+        overflow-y: scroll;
+        overflow-x: hidden;
+      }"
+    )
   ),
-  dashboardBody(
-    tabsetPanel(
-      tabPanel("Table", reactableOutput("table")),
-      tabPanel("Scatter Plots", uiOutput("scatterPlotTabs")),
-      tabPanel("Histogram", plotlyOutput("histogram"))
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("file1", "Please choose a CSV file",
+                multiple = TRUE,
+                accept = c("text/csv",
+                           "text/comma-separated-values,text/plain",
+                           ".csv")),
+      uiOutput("orgInput"), # Dynamic UI for organisms
+      uiOutput("traitInput"), # Dynamic UI for traits
+      uiOutput("chrInput") # Dynamic UI for chromosomes
+    ),
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Scatter Plot", div(class = "scrollable_plot", plotlyOutput("scatterPlot"))),
+        tabPanel("Histogram", plotlyOutput("histogramPlot"))
+      )
     )
   )
 )
 
-# Define server logic
-server <- function(input, output) {
+# Server logic
+server <- function(input, output, session) {
   
-  # Reactive expression to create a subset and sort
-  dataSubset <- reactive({
-    data <- snps_subset
-    
-    # Filter by trait if requested
-    if (!is.null(input$filter_trait) && length(input$filter_trait) > 0) {
-      data <- data[data$trait %in% input$filter_trait,]
-    }
-    
-    # Filter by species if requested
-    if (!is.null(input$filter_species) && length(input$filter_species) > 0) {
-      data <- data[data$org %in% input$filter_species,]
-    }
-    
-    # Filter by chromosome if requested
-    if (input$chromosome != "All") {
-      data <- data[data$chr == input$chromosome,]
-    }
-    
-    # Sort the data
-    data <- data[order(data[[input$sort]]),]
-    
-    # Limit the number of rows
-    data <- head(data, input$rows)
-    
-    data
+  # Reactive expression to read in the data when uploaded
+  data <- reactive({
+    req(input$file1)
+    read_csv(input$file1$datapath)
   })
   
-  # Create the table output object
-  output$table <- renderReactable({
-    reactable(dataSubset())
+  # Making big Palette for traits
+  bigPalette <- colorRampPalette(brewer.pal(12, "Paired"))
+  subPalette <- bigPalette(21)
+  
+  # Dynamic UI for organism selection
+  output$orgInput <- renderUI({
+    selectInput("org", "Select Organism(s)",
+                choices = unique(data()$org), multiple = TRUE)
   })
   
-  # Create individual scatter plots for each species
-  output$scatterPlotTabs <- renderUI({
-    lapply(unique_species, function(species) {
-      plotlyOutput(paste0("scatterPlot_", species))
-    })
+  # Dynamic UI for trait selection
+  output$traitInput <- renderUI({
+    selectInput("trait", "Select Trait(s)",
+                choices = unique(data()$trait), multiple = TRUE)
   })
   
-  observe({
-    lapply(unique_species, function(species) {
-      output[[paste0("scatterPlot_", species)]] <- renderPlotly({
-        filtered_data <- dataSubset()
-        filtered_data <- filtered_data[filtered_data$org == species, ]
-        
-        plot_ly(filtered_data, x = ~chr, y = ~bp, type = 'scatter', mode = 'markers', text = ~trait, hoverinfo = 'text') %>%
-          layout(xaxis = list(title = "Chromosome"), yaxis = list(title = "BP"))
-      })
-    })
+  # Dynamic UI for chromosome selection
+  output$chrInput <- renderUI({
+    selectInput("chr", "Select Chromosome(s)",
+                choices = unique(data()$chr), multiple = TRUE)
   })
   
-  # Create histogram
-  output$histogram <- renderPlotly({
-    plot_ly(dataSubset(), x = ~genecount, type = 'histogram')
+  # Subset data based on user selection
+  selectedData <- reactive({
+    req(input$org, input$trait, input$chr)
+    data() %>%
+      filter(org %in% input$org,
+             trait %in% input$trait,
+             chr %in% input$chr)
   })
   
+  # Scatter Plot
+  output$scatterPlot <- renderPlotly({
+    req(selectedData())
+    p <- selectedData() %>%
+      ggplot(aes(y = genecount, x = trait, color = trait)) +
+      geom_boxplot() +
+      scale_color_manual(values = subPalette) +
+      facet_wrap(~org, scales = "free", ncol = 1, strip.position = "left") +
+      theme_minimal() +
+      theme(
+        plot.title = element_text(hjust = 0.5, size = 20), 
+        strip.text = element_text(size = 14, face = "bold", angle = 0), # Rotate facet labels to be horizontal
+        axis.title = element_text(size = 12),
+        axis.text = element_text(size = 10),
+        legend.text = element_text(size = 12), 
+        legend.title = element_text(size = 14)
+      ) +
+      coord_cartesian(clip = "off") # prevent clipping of strip labels
+    
+    # Increase overall plot height; adjust based on the number of facets
+    plot_height <- 300 * length(unique(selectedData()$org))
+    
+    ggplotly(p, height = plot_height, width = 800)
+  })
+  
+  
+  # Histogram
+  output$histogramPlot <- renderPlotly({
+    req(selectedData())
+    plot_ly(data = selectedData(), 
+            x = ~genecount, 
+            type = 'histogram', 
+            color = ~trait)
+  })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
